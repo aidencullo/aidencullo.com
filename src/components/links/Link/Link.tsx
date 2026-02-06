548781fcee01fabcbff71dbc78f83321dd056596
aiden cullo <aidencullo@gmail.com>
2026-02-06 16:55:14 -0500

    Open links in same tab

--- src/components/links/Link/Link.tsx
+++ src/components/links/Link/Link.tsx
@@ -17,15 +17,10 @@ const Link: React.FC<LinkProps> = ({
   className,
   download = false
 }) => {
-  const target = isExternal ? "_blank" : undefined
-  const rel = isExternal ? "noopener noreferrer" : undefined
-  
   return (
     <a 
       href={href} 
       title={title}
-      target={target}
-      rel={rel}
       className={`link ${className || ''}`}
       download={download}
     >
