352721a89ce420d9506e4b913ba8b8d2842bb55a
aiden cullo <aidencullo@gmail.com>
2026-02-06 10:40:06 -0500

    Update files

--- src/components/Body/Bio/BioText/BioText.tsx
+++ src/components/Body/Bio/BioText/BioText.tsx
@@ -6,7 +6,7 @@ const BioText: React.FC = () => {
     <div className="bio-text">
       <p>
         Brooklyn-based software engineer building a water analysis platform at{' '}
-        <a href="https://www.techtotherescue.org/">Tech to the Rescue</a>; previously built an LLM system of record at JPMC.
+        <a href="https://www.techtotherescue.org/">Tech to the Rescue</a>; previously built an LLM system of record at JP Morgan.
       </p>
     </div>
   )
