105115faeadec045c4c1d9b92b0a647f6134e06e
aiden cullo <aidencullo@gmail.com>
2026-02-10 22:49:35 -0500

    Move random welcome above profile photo

--- src/components/Body/Bio/Bio.tsx
+++ src/components/Body/Bio/Bio.tsx
@@ -1,12 +1,53 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import ProfilePicture from './ProfilePicture/ProfilePicture'
 import BioText from './BioText/BioText'
 import BioLinks from './BioLinks/BioLinks'
 import './Bio.css'
 
 const Bio: React.FC = () => {
+  const welcomes = [
+    'Welcome',
+    'Bienvenido',
+    'Bienvenue',
+    'Willkommen',
+    'Benvenuto',
+    'Bem-vindo',
+    'Velkommen',
+    'Välkommen',
+    'Tervetuloa',
+    'Witamy',
+    'Dobro došli',
+    'Tervist',
+    'Welkom',
+    'Καλώς ήρθες',
+    'Привет',
+    'مرحبا',
+    'שלום',
+    'हैलो',
+    'স্বাগতম',
+    'வணக்கம்',
+    'ನಮಸ್ಕಾರ',
+    'สวัสดี',
+    'Xin chào',
+    '你好',
+    'こんにちは',
+    '안녕하세요',
+    'Mabuhay',
+    'Sawubona',
+    'Karibu',
+    'Salam'
+  ]
+
+  const [welcome, setWelcome] = useState('Welcome')
+
+  useEffect(() => {
+    const next = welcomes[Math.floor(Math.random() * welcomes.length)]
+    setWelcome(next)
+  }, [])
+
   return (
     <div className="bio-container" id="bio">
+      <div className="bio-welcome">{welcome}</div>
       <ProfilePicture />
       <BioText />
       <BioLinks />
--- src/components/Header/HeaderRight/HeaderRight.tsx
+++ src/components/Header/HeaderRight/HeaderRight.tsx
@@ -1,51 +1,10 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import ThemeButton from './ThemeButton/ThemeButton'
 import './HeaderRight.css'
 
 const HeaderRight: React.FC = () => {
-  const welcomes = [
-    'Welcome',
-    'Bienvenido',
-    'Bienvenue',
-    'Willkommen',
-    'Benvenuto',
-    'Bem-vindo',
-    'Velkommen',
-    'Välkommen',
-    'Tervetuloa',
-    'Witamy',
-    'Dobro došli',
-    'Tervist',
-    'Welkom',
-    'Καλώς ήρθες',
-    'Привет',
-    'مرحبا',
-    'שלום',
-    'हैलो',
-    'স্বাগতম',
-    'வணக்கம்',
-    'ನಮಸ್ಕಾರ',
-    'สวัสดี',
-    'Xin chào',
-    '你好',
-    'こんにちは',
-    '안녕하세요',
-    'Mabuhay',
-    'Sawubona',
-    'Karibu',
-    'Salam'
-  ]
-
-  const [welcome, setWelcome] = useState('Welcome')
-
-  useEffect(() => {
-    const next = welcomes[Math.floor(Math.random() * welcomes.length)]
-    setWelcome(next)
-  }, [])
-
   return (
     <div className="header-right">
-      <span className="header-welcome">{welcome}</span>
       <ThemeButton />
     </div>
   )
