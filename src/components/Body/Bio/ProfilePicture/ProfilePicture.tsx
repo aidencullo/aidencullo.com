e75d06cedd1c9b3a986dbb72466efb657f47fdc9
aiden cullo <aidencullo@gmail.com>
2026-02-17 14:22:33 -0500

    Link profile picture to Flickr

--- src/components/Body/Bio/ProfilePicture/ProfilePicture.tsx
+++ src/components/Body/Bio/ProfilePicture/ProfilePicture.tsx
@@ -4,10 +4,13 @@ import './ProfilePicture.css'
 const ProfilePicture: React.FC = () => {
   const imagePath = "/profile.jpg"
   const altText = "Aiden Cullo"
+  const flickrUrl = "https://www.flickr.com/photos/136772980@N04/"
   
   return (
     <div className="profile-picture">
-      <img src={imagePath} alt={altText} />
+      <a href={flickrUrl} target="_blank" rel="noopener noreferrer">
+        <img src={imagePath} alt={altText} />
+      </a>
     </div>
   )
 }
