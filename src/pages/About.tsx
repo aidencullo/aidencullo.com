e6965069e828bfc7e589a682e371c3335227a10d
aiden cullo <aidencullo@gmail.com>
2026-03-23 16:24:03 -0500

    Add mission statement paragraph to About page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -11,6 +11,15 @@ const About: React.FC = () => {
         and data pipelines. I care about writing clean code and using technology
         to make the world a better place.
       </p>
+      <p>
+        What I'm looking for is ways to leverage technology to protect vulnerable
+        populations and promote equality and justice. I'm currently exploring how
+        we can achieve this — is it through developing responsible AI and trusting
+        it? A different technology, a more transparent rendition of AI? Or can we
+        build non-AI safeguards for AI? I want to work at the intersection of
+        software, AI, and human rights to preserve and strengthen the democratic
+        institutions we have and bolster human agency and cooperation.
+      </p>
 
       <h2>What I Do</h2>
       <ul>
