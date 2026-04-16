b95f3cdd785290a352c78edba303e4bb8438956f
Aiden Cullo <aidencullo@users.noreply.github.com>
2026-04-16 19:07:21 -0400

    Remove footer copyright text

--- src/components/Footer/FooterBottom/FooterBottom.tsx
+++ src/components/Footer/FooterBottom/FooterBottom.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import FooterBottomLinks from './FooterBottomLinks/FooterBottomLinks';
-import Copyright from './Copyright/Copyright';
 import './FooterBottom.css';
 
 const FooterBottom: React.FC = () => {
   return (
     <div className="footer-bottom">
       <FooterBottomLinks />
-      <Copyright />
     </div>
   );
 };
--- src/components/SiteFooter/SiteFooter.tsx
+++ src/components/SiteFooter/SiteFooter.tsx
@@ -11,7 +11,6 @@ const SiteFooter: React.FC = () => {
         <a href="https://github.com/aidencullo" target="_blank" rel="noopener noreferrer">GitHub</a>
         <a href="https://www.linkedin.com/in/aidencullo" target="_blank" rel="noopener noreferrer">LinkedIn</a>
       </div>
-      <div className="footer-copy">&copy; {new Date().getFullYear()} Aiden Cullo</div>
       <div className="footer-powered">
         Hosted on GitHub Pages.
       </div>
